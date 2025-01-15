9546b796551fc5d68b49fff6939fa10a4af5395f
Adrien Vannson <adrien.vannson@protonmail.com>
2025-01-15 19:04:49 +0100

    Generate additional methods for the Any type

---
+++ src/betterproto2_compiler/known_types/__init__.py
@@ -0,0 +1,10 @@
+from collections.abc import Callable
+
+from .any import Any
+
+# For each (package, message name), lists the methods that should be added to the message definition.
+# The source code of the method is read from the `known_types` folder. If imports are needed, they can be directly added
+# to the template file: they will automatically be removed if not necessary.
+KNOWN_METHODS: dict[tuple[str, str], Callable] = {
+    ("google.protobuf", "Any"): [Any.pack, Any.unpack, Any.to_dict],
+}
