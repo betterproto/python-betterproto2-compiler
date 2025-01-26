8a76fab489479d392694249d49293f11d85ccbcc
Adrien Vannson <adrien.vannson@protonmail.com>
2025-01-26 23:15:20 +0100

    Remove generated code to pass typechecking

--- src/betterproto2_compiler/lib/google/protobuf/__init__.py
+++ src/betterproto2_compiler/lib/google/protobuf/__init__.py
@@ -724,35 +724,6 @@ class Any(betterproto2.Message):
     Must be a valid serialized protocol buffer of the above specified type.
     """
 
-    def pack(self, message: betterproto2.Message, message_pool: "betterproto2.MessagePool | None" = None) -> None:
-        """
-        Pack the given message in the `Any` object.
-
-        The message type must be registered in the message pool, which is done automatically when the module defining
-        the message type is imported.
-        """
-        message_pool = message_pool or default_message_pool
-
-        self.type_url = message_pool.type_to_url[type(message)]
-        self.value = bytes(message)
-
-    def unpack(self, message_pool: "betterproto2.MessagePool | None" = None) -> betterproto2.Message:
-        """
-        Return the message packed inside the `Any` object.
-
-        The target message type must be registered in the message pool, which is done automatically when the module
-        defining the message type is imported.
-        """
-        message_pool = message_pool or default_message_pool
-
-        message_type = message_pool.url_to_type[self.type_url]
-
-        return message_type().parse(self.value)
-
-    def to_dict(self) -> dict:  # pyright: ignore [reportIncompatibleMethodOverride]
-        # TOOO improve when dict is updated
-        return {"@type": self.type_url, "value": self.unpack().to_dict()}
-
 
 default_message_pool.register_message("google.protobuf", "Any", Any)
 
