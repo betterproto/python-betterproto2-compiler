3d85696c5dee9b289472765e5b912badad2188e4
Adrien Vannson <adrien.vannson@protonmail.com>
2025-04-06 11:45:45 +0200

    Fix import for Python 3.10

--- src/betterproto2_compiler/known_types/google_values.py
+++ src/betterproto2_compiler/known_types/google_values.py
@@ -1,14 +1,17 @@
-import typing
+from typing import TYPE_CHECKING
 
 from betterproto2_compiler.lib.google.protobuf import (
     BoolValue as VanillaBoolValue,
     StringValue as VanillaStringValue,
 )
 
+if TYPE_CHECKING:
+    from typing import Self
+
 
 class BoolValue(VanillaBoolValue):
     @staticmethod
-    def from_wrapped(wrapped: bool) -> typing.Self:
+    def from_wrapped(wrapped: bool) -> "Self":
         return BoolValue(value=wrapped)
 
     def to_wrapped(self) -> bool:
@@ -17,7 +20,7 @@ class BoolValue(VanillaBoolValue):
 
 class StringValue(VanillaStringValue):
     @staticmethod
-    def from_wrapped(wrapped: str) -> typing.Self:
+    def from_wrapped(wrapped: str) -> "Self":
         return StringValue(value=wrapped)
 
     def to_wrapped(self) -> str:
